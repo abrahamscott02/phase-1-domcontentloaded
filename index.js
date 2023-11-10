247427b9e0243b7ea8caa7521c1886199c93f045
Abraham Scott <abrahamscott02@gmail.com>
2023-11-10 12:13:51 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', targetElementId)
+
+function targetElementId(){
+    let getId = document.getElementById('text')
+    getId.innerHTML = "This is really cool!"
+}
